87b9569e83e47a26894a243130a616a137c55034
Miyuru Dharmage <miyuru@theredsun.org>
2026-03-23 09:19:07 +0530

    fix: change shelf_id column type from UUID to TEXT in grns table

--- packages/backend/prisma/migrations/20260322060000_add_shelf_to_grn/migration.sql
+++ packages/backend/prisma/migrations/20260322060000_add_shelf_to_grn/migration.sql
@@ -1,3 +1,3 @@
 -- Add shelf_id to grns table
-ALTER TABLE "grns" ADD COLUMN "shelf_id" UUID;
+ALTER TABLE "grns" ADD COLUMN "shelf_id" TEXT;
 ALTER TABLE "grns" ADD CONSTRAINT "grns_shelf_id_fkey" FOREIGN KEY ("shelf_id") REFERENCES "shelves"("id") ON DELETE SET NULL ON UPDATE CASCADE;
