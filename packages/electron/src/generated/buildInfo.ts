1478a7495b1727b1207931013b9ced981e9a86d8
Miyuru <voxsar@gmail.com>
2026-05-20 16:27:49 +0530

    chore: update build information for version 1.0.2

--- packages/electron/src/generated/buildInfo.ts
+++ packages/electron/src/generated/buildInfo.ts
@@ -1,8 +1,8 @@
 export const GENERATED_BUILD_INFO = {
   "packageName": "@jingles/electron",
   "appVersion": "1.0.2",
-  "buildNumber": "298",
-  "commitHash": "4d6aab52c02b461932159120a2e590901edfadda",
-  "commitShortHash": "4d6aab52c02b",
-  "builtAt": "2026-05-20T09:49:58.621Z"
+  "buildNumber": "299",
+  "commitHash": "a96988e256194b5219bfad9e91602bb1bfbfd41f",
+  "commitShortHash": "a96988e25619",
+  "builtAt": "2026-05-20T10:03:39.588Z"
 } as const;
