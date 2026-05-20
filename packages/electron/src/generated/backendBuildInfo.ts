71bcbd56c4617ae34354e101476cc65378cc3d41
Miyuru <voxsar@gmail.com>
2026-05-20 17:10:12 +0530

    chore: update build information for version 1.0.2

--- packages/electron/src/generated/backendBuildInfo.ts
+++ packages/electron/src/generated/backendBuildInfo.ts
@@ -1,8 +1,8 @@
 export const GENERATED_BUILD_INFO = {
   "packageName": "@jingles/backend",
   "appVersion": "1.0.2",
-  "buildNumber": "302",
-  "commitHash": "07df2c0a08fd7622ceadab2cb6c2a92dad8c54e0",
-  "commitShortHash": "07df2c0a08fd",
-  "builtAt": "2026-05-20T11:26:35.217Z"
+  "buildNumber": "304",
+  "commitHash": "bae9f4d9c6259740f33478b56acc612d7046cd29",
+  "commitShortHash": "bae9f4d9c625",
+  "builtAt": "2026-05-20T11:39:53.523Z"
 } as const;
