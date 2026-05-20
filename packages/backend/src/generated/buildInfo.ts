13dbb5d4bf8753a6d1b9d1d09aa04db9ab932610
Miyuru <voxsar@gmail.com>
2026-05-21 00:18:45 +0530

    chore: remove obsolete build information files

--- packages/backend/src/generated/buildInfo.ts
+++
@@ -1,8 +0,0 @@
-export const GENERATED_BUILD_INFO = {
-  "packageName": "@jingles/backend",
-  "appVersion": "1.0.2",
-  "buildNumber": "303",
-  "commitHash": "a2f015ff3354b9b1006249b59629c2d8e03eec66",
-  "commitShortHash": "a2f015ff3354",
-  "builtAt": "2026-05-20T11:38:01.828Z"
-} as const;
--- packages/electron/src/generated/buildInfo.ts
+++
@@ -1,8 +0,0 @@
-export const GENERATED_BUILD_INFO = {
-  "packageName": "@jingles/electron",
-  "appVersion": "1.0.2",
-  "buildNumber": "304",
-  "commitHash": "bae9f4d9c6259740f33478b56acc612d7046cd29",
-  "commitShortHash": "bae9f4d9c625",
-  "builtAt": "2026-05-20T11:39:53.453Z"
-} as const;
